c4ec2774794a0a71495bc6676f3e2457282f1abe
genie360s <alexgmkwizu@gmail.com>
2024-01-07 15:16:27 +0300

    fixed port number variable display on the terminal

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use(express.static('public'));
 
 // Start the server
 app.listen(port, () => {
-  console.log("Server running at http://localhost:${port}/");
+  console.log(`Server running at http://localhost:${port}/`);
 });
